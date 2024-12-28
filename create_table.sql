52625df56d0dd8306f367534ebe6eb734f44a081
ssp-adb <153721996+ssp-adb@users.noreply.github.com>
2024-12-28 23:02:38 +0800

    Add files via upload

--- create_table.sql
+++ create_table.sql
@@ -1,3 +1,39 @@
+CREATE TABLE game_table (
+    game_id INT PRIMARY KEY,
+    game_name VARCHAR(200),
+    publisher VARCHAR(50),
+    developer VARCHAR(200),
+    platform_id INT,
+    rating VARCHAR(10)
+);
+
+CREATE TABLE platform_table (
+    platform_id INT PRIMARY KEY,
+    platform_name VARCHAR(50)
+);
+
+CREATE TABLE rating_table (
+    rating VARCHAR(10) PRIMARY KEY,
+    age INT
+);
+
+CREATE TABLE score_table (
+    game_id INT PRIMARY KEY,
+    critic_score FLOAT,
+    critic_count INT,
+    user_score FLOAT,
+    user_count INT
+);
+
+CREATE TABLE sales_table (
+    game_id INT PRIMARY KEY,
+    na_sales FLOAT,
+    eu_sales FLOAT,
+    jp_sales FLOAT,
+    other_sales FLOAT,
+    global_sales FLOAT
+);
+
 CREATE TABLE comment_table (
     comment_id INT AUTO_INCREMENT PRIMARY KEY,
     game_id INT NOT NULL,
